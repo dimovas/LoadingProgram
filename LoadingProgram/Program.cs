f94dbd456be7499817f136dcc95a029fb7a425c5
Светлана Леонова <svetlana.dmitrenko@gmail.com>
2019-05-09 10:25:03 +0300

    new

--- LoadingProgram/Program.cs
+++ LoadingProgram/Program.cs
@@ -10,6 +10,7 @@ namespace LoadingProgram
     {
         static void Main(string[] args)
         {
+            //Hydrostatics table for some try
             double[,] sh = new double[8, 5];
             sh[0, 0] = 1.9; sh[0,1] = 3081.6; sh[0, 2] = 3006.4; sh[0, 3] = 3.636; sh[0, 4] = 1.022;
             sh[1, 0] = 3.0; sh[1,1] = 5100.3; sh[1, 2] = 4976.1; sh[1, 3] = 3.771; sh[1, 4] = 1.588;
@@ -20,8 +21,10 @@ namespace LoadingProgram
             sh[6, 0] = 8.0; sh[6,1] = 14720.2; sh[6, 2] = 14361.1; sh[6, 3] = 2.832; sh[6, 4] = 4.172;
             sh[7, 0] = 9.0; sh[7,1] = 16794.4; sh[7, 2] = 16384.8; sh[7, 3] = 2.309; sh[7, 4] = 4.706;
 
+            //Input the ship displasement
             double shipDispl = 13225;
 
+            //Find the string index of the nearest displacement which is smaller than given one
             int m = -1, n = 2;
             for (int i = 0; i < sh.GetLength(0); i++)
             {
@@ -31,16 +34,18 @@ namespace LoadingProgram
                     break;
                 }
             }
+            // Write the indexex of nearest displacement elements
             // Console.WriteLine($"D1 = {sh[m, n]}\tD2 = {sh[(m + 1), n]}");
+
+            //Find the draught of the ship using INTERPOLATON 
             double draughtWL = sh[m, 0] + (shipDispl - sh[m, n]) * (sh[(m + 1), 0] - sh[m, 0]) / (sh[(m + 1), n] - sh[m, n]);
             Console.WriteLine($"Draught at center of WL = {draughtWL}");
 
-            //FindDrfatCWL(shipDispl);
+           
             Console.WriteLine("Input file adress");
             string filename = Console.ReadLine();
-            LoadCSV(filename);
 
-
+            LoadCSV(filename);//
         }
 
         private static double[,] LoadCSV(string filename)
@@ -61,22 +66,6 @@ namespace LoadingProgram
 
             return values;
         }
-
-        /* private static double FindDrfatCWL(double shipDispl)
-         {
-             int m = -1, n = 2;
-             for (int i = 0; i < sh.GetLength(0); i++)
-             {
-                 if (sh[i, n] > shipDispl / 1.025)
-                 {
-                     m = i - 1;
-                     break;
-                 }
-             }
-             double draughtWL = sh[m, 0] + (shipDispl - sh[m, n]) * (sh[(m + 1), 0] - sh[m, 0]) / (sh[(m + 1), n] - sh[m, n]);
-             Console.WriteLine($"Draught at center of WL = {draughtWL}");
-             return draughtWL;
-         }
-         */
+       
     }
 }
